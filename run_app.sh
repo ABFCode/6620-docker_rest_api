826f5f1836b8cb149c25b5bd3b43d869c89adc89
Aidan Finnegan <71090977+ABFCode@users.noreply.github.com>
2025-06-20 20:41:47 -0400

    docker app run

---
+++ run_app.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Building the API Docker image..."
+docker build -t my-book-app -f Dockerfile.app .
+
+echo "Running the API container..."
+docker run -p 5000:5000 --rm my-book-app
